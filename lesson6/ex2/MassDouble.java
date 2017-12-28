16174fac27f695792b75475b20614d25ec85cddc
Bilmo <v.zel.sky@gmail.com>
2017-12-28 21:17:32 +0200

    fix

--- lesson6/ex2/MassDouble.java
+++
@@ -1,8 +0,0 @@
-package ex2;
-
-public class MassDouble {
-    public static void main(String[] args) {
-        double [] array = { 22.6 , 26.5 , 35.9 , 59.5};
-        System.out.println(array[0]);
-    }
-}
