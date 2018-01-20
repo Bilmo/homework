5e4f813dbd2837e28b8d741dd8362dd3f09108d8
Bilmo <v.zel.sky@gmail.com>
2018-01-20 18:10:14 +0200

    hw

--- src/lesson7/com/company/vehicles/SportCar.java
+++ src/lesson7/com/company/vehicles/SportCar.java
@@ -3,12 +3,12 @@ package lesson7.com.company.vehicles;
 import lesson7.com.company.details.Engine;
 import lesson7.com.company.professions.Driver;
 
-public class Lorry extends Car {
-    private int carrying;
+public class SportCar extends Car {
+    private double speed;
 
 
-    public Lorry(String marka, String carClass, double weight, Driver driver, Engine engine) {
+    public SportCar(String marka, String carClass, double weight, Driver driver, Engine engine) {
         super(marka, carClass, weight, driver, engine);
-        this.carrying = carrying;
+        this.speed = speed;
     }
 }
